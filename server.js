1970336052c96b14bf9e939f690ed91eebc0f740
jheal006 <jheal006@gmail.com>
2017-10-10 22:35:40 -0700

    Update server.js

--- server.js
+++ server.js
@@ -1,19 +1,50 @@
 // Dependencies
 var express = require("express");
-var mongojs = require("mongojs");
+var bodyParser = require("body-parser");
+var logger = require("morgan");
+var mongoose = require("mongoose");
+
+// Scripting Tools
+var cheerio = require("cheerio");
+var request = require("request");
+
+//Require Models
+var Aricle = require("./models/Article.js");
+
+// set mongoose to leverage built in JS ES6 Promises
+mongoose.Promise = Promise;
 
 // Initialize Express
 var app = express();
 
-// Parses our HTML and helps us find elements
-var cheerio = require("cheerio");
-// Makes HTTP request for HTML page
-var request = require("request");
+// Use morgan and body parser with our app
+app.use(logger("dev"));
+app.use(bodyParser.urlencoded({
+  extended: false
+}));
+
+// Make public a static dir
+app.use(express.static("public"));
+
+// Database configuration with mongoose
+mongoose.connect("mongodb://localhost/test");
+var db = mongoose.connection;
+
+// Show any mongoose errors
+db.on("error", function(error) {
+  console.log("Mongoose Error: ", error);
+});
+
+// Once logged in to the db through mongoose, log a success message
+db.once("open", function() {
+  console.log("Mongoose connection successful.");
+});
+
 
 // Routes
 // =============================================================
 
-require("./controllers/index.js")(app);
+require("./routes/news.js")(app);
 
 
 // First, tell the console what server.js is doing
